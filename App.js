f2cd97574e1706d9aa8b2ba5c4a98d4951204be3
AGLOP-1354 <82454882+AGLOP-1354@users.noreply.github.com>
2021-06-24 16:04:08 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Alert} from "react-native";
+import Loading from './Loading';
+import * as Location from "expo-location";
+import axios from 'axios';
+import Weather from './Weather';
+import { WebView } from 'react-native-webview';
+
+const API_KEY = "9b70ed48914142fbe44d06c117785613";
+
+export default class extends React.Component {
+  state = {
+    isLoading :true
+  };
+  getWeather = async(latitude, longitude) =>{
+    const {
+      data : {
+        main:{temp},
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+      );
+      this.setState({
+        isLoading:false, 
+        condition : weather[0].main, 
+        temp
+      });
+  }
+  getLocation = async() =>{
+    try{
+      await Location.requestForegroundPermissionsAsync();
+      const {coords: {latitude, longitude}} = 
+      await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error){
+      Alert.alert("Can't find you", "So Sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    <WebView source={{ uri: 'exp://exp.host/@aglop/weather-app' }} style={{ marginTop: 20 }} />;
+    const {isLoading, temp, condition} = this.state;
+    return(
+      isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />
+    )
+  }
+}
